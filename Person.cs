d2c37355fd221a8400537c819afc39c4e8e905b8
hmarci <hecks.marton2@gmail.com@petrik.hu>
2026-03-20 10:13:06 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,58 @@
+﻿using static CitySimproj.Npclib;
+
+namespace CitySimproj
+{
+	internal class Person
+	{
+		private static Random random = new Random(); 
+
+		protected string name;
+		protected int id;
+		protected int age;
+		protected bool sex;
+		private Traits traits;
+		private Race race;
+
+		public string Name { get => name; set => name = value; }
+		public int Age { get => age; set => age = value; }
+		public bool Sex { get => sex; set => sex = value; }
+		public int Id { get => id; set => id = value; }
+		public Traits Traits { get => traits; set => traits = value; } 
+		public Race Race { get => race; set => race = value; }     
+
+		public Person(int id)
+		{
+			this.id = id;
+
+			Npclib lib = new Npclib();
+
+			this.sex = random.Next(2) == 0; //Random sex
+
+			this.age = random.Next(1, 101); //Random Age
+
+			string surname = lib.surnames[random.Next(lib.surnames.Length)]; //Random Surname
+
+			string firstname = sex
+				? lib.male_fornames[random.Next(lib.male_fornames.Length)]
+				: lib.female_fornames[random.Next(lib.female_fornames.Length)]; //Sex alapján Firstname
+
+			this.name = firstname + " " + surname;
+
+			this.race = (Race)random.Next(Enum.GetValues(typeof(Race)).Length); //Random race
+			this.traits = (Traits)random.Next(Enum.GetValues(typeof(Traits)).Length); //Random Trait
+		}
+		public void NPC()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Person p = new Person(i);
+				Console.WriteLine(p);
+			}
+		}
+		public override string ToString()
+		{
+			return $"Name: {this.name}, Age: {this.age}, ID: {this.id} {(this.sex ? "male" : "female")}, Race: {this.race}, Traits: {this.traits}"; //Kiiratás
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CitySimproj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+		}
+    }
+}
