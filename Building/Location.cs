f8dbc8f76c5ea41daab5a57433cb5137bc39682c
Vince <vini.molnar@gmail.com>
2026-04-15 22:58:54 +0200

    Minden csapat munkáját egy mappába tettem átlathtóság kedvéért, modosításokat nem végeztem el(kivéve a Buildingnél) minden működik.

---
+++ Building/Location.cs
@@ -0,0 +1,18 @@
+﻿namespace Buildings
+{
+	public class Location
+    {
+		private Building building;
+
+		private int x;
+		private int y;
+
+        public Location(Building building, int x, int y)
+        {
+            this.building = building;
+            this.x = x;
+            this.y = y;
+        }
+    }
+    //Nincs használva, de a későbbiekben lehet, hogy szükség lesz rá, ezért létrehoztam egy külön osztályt a helyeknek, ahol az épületek állnak, így könnyebben kezelhető lesz a város térképe
+}
