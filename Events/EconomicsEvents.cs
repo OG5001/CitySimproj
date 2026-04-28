47ff3119a2e23fed00150ac4c8a489804629dadb
Your Name <you@example.com>
2026-04-28 08:15:20 +0200

    basic template for economics events + economics events blueprint

---
+++ Events/EconomicsEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Buildings;
+
+
+namespace CitySimproj
+{
+    // --- ECONOMICS EVENTS ---
+
+    
+
+    // Explanation
+
+
+}
---
+++ Events/EconomicsEventsBlueprint.cs
@@ -0,0 +1,31 @@
+﻿using Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitySimproj
+{
+
+    // The Blueprint class for Natural Disasters.
+    abstract class EconomicsEventsBlueprint
+    {
+        private string name;
+       
+        public EconomicsEventsBlueprint(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void StartEffect()
+        {
+          
+        }
+
+        // --- Explanation ---
+      
+    }
+
+}
