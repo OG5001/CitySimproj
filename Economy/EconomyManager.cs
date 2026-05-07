5c6b74d186a00a27248b9821baa08da49538267b
Zetisz <szabo.zeteny@petrik.hu>
2026-05-07 18:37:29 +0200

    Fixed SelectAmount UI flicker

--- Economy/EconomyManager.cs
+++ Economy/EconomyManager.cs
@@ -290,57 +290,56 @@ internal class EconomyManager(Treasury t)
     {
         int amount = min;
 
+        Console.Clear();
+
+        int windowWidth = Console.WindowWidth;
+        int startY = 3;
+
+        // Draw title only
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        for (int i = 0; i < title.Length; i++)
+        {
+            int x = (windowWidth - title[i].Length) / 2;
+
+            Console.SetCursorPosition(x, startY + i);
+            Console.Write(title[i]);
+        }
+
+        Console.ResetColor();
+
+        int y2 = startY + title.Length + 3;
+
+        WriteCentered("< > to change | Enter to confirm", ConsoleColor.DarkGray);
+
         while (true)
         {
-            Console.Clear();
-
-            int windowWidth = Console.WindowWidth;
-
-            int startY = 3;
-
-            // Draw title
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            for (int i = 0; i < title.Length; i++)
-            {
-                int x = (windowWidth - title[i].Length) / 2;
-
-                Console.SetCursorPosition(x, startY + i);
-                Console.Write(title[i]);
-            }
-
-            Console.ResetColor();
-
-            // Amount selector
             string text = $"< {amount} >";
 
             int x2 = (windowWidth - text.Length) / 2;
-            int y2 = startY + title.Length + 3;
 
+            // Clear only the old amount line
+            Console.SetCursorPosition(0, y2);
+            Console.Write(new string(' ', windowWidth));
+
+            // Draw updated amount
             Console.SetCursorPosition(x2, y2);
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write(text);
-
             Console.ResetColor();
-
-            WriteCentered("< > to change | Enter to confirm", ConsoleColor.DarkGray);
 
             var key = Console.ReadKey(true).Key;
 
             if (key == ConsoleKey.LeftArrow)
             {
-                amount--;
-
-                if (amount < min)
-                    amount = min;
+                if (amount > min)
+                    amount--;
             }
             else if (key == ConsoleKey.RightArrow)
             {
-                amount++;
-
-                if (amount > max)
-                    amount = max;
+                if (amount < max)
+                    amount++;
             }
             else if (key == ConsoleKey.Enter)
             {
